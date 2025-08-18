6bd4ac6fe833cf65476446ebf2bf79ff49d0e97f
Zane Reid <zane.reid@infinity.com.au>
2025-08-18 10:35:07 +1000

    Added on logo for branding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	import colorLogo from '../assets/infinityLogo.png';
+
 	let availableCapital = $state(0);
 	let personalOrSMSF = $state('personal');
 	let smsfCashNeeded = $state(0);
@@ -65,146 +67,150 @@ Capital Surplus: ${-capitalGap}
 </script>
 
 <main class="container mx-auto max-w-md p-8">
-	<div id="pdf-content">
-		<h1 class="mb-4 text-center text-3xl font-bold">The chad time save Liquidity Calculator</h1>
-		<h3 class="mb-3 text-center text-xl font-semibold">dont ask me stupid questions idiot!!!</h3>
+	<div class="flex w-full items-center justify-between py-2">
+		<div id="pdf-content">
+			<img src={colorLogo} alt="infinity-logo" class=" m-auto h-12" />
+			<h1 class="mb-4 text-center text-2xl font-bold">The Liquidity Calculator</h1>
 
-		<div class="space-y-4">
-			<div>
-				<label class="mb-2 block text-sm font-medium" for="available-capital"
-					>How much capital do you have available?</label
-				>
-				<input
-					type="number"
-					bind:value={availableCapital}
-					placeholder="e.g. 250000"
-					class="w-full rounded-md border border-gray-300 p-3 focus:ring-2 focus:ring-blue-500"
-				/>
-			</div>
-
-			<div>
-				<label class="mb-2 block text-sm font-medium" for="personal-or-smsf"
-					>Personal or SMSF?</label
-				>
-				<select
-					bind:value={personalOrSMSF}
-					class="w-full rounded-md border border-gray-300 p-3 focus:ring-2 focus:ring-blue-500"
-				>
-					<option value="personal">Personal</option>
-					<option value="smsf">SMSF</option>
-				</select>
-			</div>
-
-			{#if personalOrSMSF === 'smsf'}
+			<div class="space-y-4">
 				<div>
-					<label class="mb-2 block text-sm font-medium" for="smsf-cash-needed">
-						How much cash are you required to maintain for liquidity?
-					</label>
+					<label class="mb-2 block text-sm font-medium" for="available-capital"
+						>How much capital do you have available?</label
+					>
 					<input
 						type="number"
-						bind:value={smsfCashNeeded}
+						bind:value={availableCapital}
+						placeholder="e.g. 250000"
+						class="w-full rounded-md border border-gray-300 p-3 focus:ring-2 focus:ring-blue-500"
+					/>
+				</div>
+
+				<div>
+					<label class="mb-2 block text-sm font-medium" for="personal-or-smsf"
+						>Personal or SMSF?</label
+					>
+					<select
+						bind:value={personalOrSMSF}
+						class="w-full rounded-md border border-gray-300 p-3 focus:ring-2 focus:ring-blue-500"
+					>
+						<option value="personal">Personal</option>
+						<option value="smsf">SMSF</option>
+					</select>
+				</div>
+
+				{#if personalOrSMSF === 'smsf'}
+					<div>
+						<label class="mb-2 block text-sm font-medium" for="smsf-cash-needed">
+							How much cash are you required to maintain for liquidity?
+						</label>
+						<input
+							type="number"
+							bind:value={smsfCashNeeded}
+							placeholder="e.g. 100000"
+							class="w-full rounded-md border border-gray-300 p-3 focus:ring-2 focus:ring-blue-500"
+						/>
+					</div>
+				{/if}
+
+				<div>
+					<label class="mb-2 block text-sm font-medium" for="remaining-deposits"
+						>Remaining deposits:</label
+					>
+					<input
+						type="number"
+						bind:value={remainingDeposits}
 						placeholder="e.g. 100000"
 						class="w-full rounded-md border border-gray-300 p-3 focus:ring-2 focus:ring-blue-500"
 					/>
 				</div>
-			{/if}
 
-			<div>
-				<label class="mb-2 block text-sm font-medium" for="remaining-deposits"
-					>Remaining deposits:</label
+				<div>
+					<label class="mb-2 block text-sm font-medium" for="solicitors-fees"
+						>Solicitors fees:</label
+					>
+					<input
+						type="number"
+						bind:value={solicitorsFees}
+						placeholder="e.g. 100000"
+						class="w-full rounded-md border border-gray-300 p-3 focus:ring-2 focus:ring-blue-500"
+					/>
+				</div>
+
+				<div>
+					<label class="mb-2 block text-sm font-medium" for="stamp-duty"
+						>Stamp duty & other transfer costs:</label
+					>
+					<input
+						type="number"
+						bind:value={stampDuty}
+						placeholder="e.g. 100000"
+						class="w-full rounded-md border border-gray-300 p-3 focus:ring-2 focus:ring-blue-500"
+					/>
+				</div>
+
+				<div>
+					<label class="mb-2 block text-sm font-medium" for="construction-costs"
+						>Construction costs (if not applicable leave at 0):</label
+					>
+					<input
+						type="number"
+						bind:value={constructionCosts}
+						placeholder="e.g. 100000"
+						class="w-full rounded-md border border-gray-300 p-3 focus:ring-2 focus:ring-blue-500"
+					/>
+				</div>
+
+				<div>
+					<label class="mb-2 block text-sm font-medium" for="contingency-capital"
+						>Contingency capital:</label
+					>
+					<input
+						type="number"
+						bind:value={contingencyCapital}
+						placeholder="e.g. 100000"
+						class="w-full rounded-md border border-gray-300 p-3 focus:ring-2 focus:ring-blue-500"
+					/>
+				</div>
+
+				<div>
+					<label class="mb-2 block text-sm font-medium" for="settlement-date"
+						>Next projected property settlement date:</label
+					>
+					<input
+						type="date"
+						bind:value={settlementDate}
+						class="w-full rounded-md border border-gray-300 p-3 focus:ring-2 focus:ring-blue-500"
+					/>
+				</div>
+
+				<div class="space-y-2 rounded-md bg-gray-100 p-4">
+					<p class="font-medium">
+						Amount required for your current strategy: ${totalCapitalRequired}
+					</p>
+					<p>
+						{#if monthsBetween <= 12 && monthsBetween >= 0}
+							Capital required in 12 months: ${capitalRequired12Months.toLocaleString()}
+						{/if}
+					</p>
+					<p>
+						{#if monthsBetween >= 12}
+							Capital required in 24 Months or Moree: ${capitalRequired24Months.toLocaleString()}
+						{/if}
+					</p>
+					<p class={capitalGap > 0 ? 'font-semibold text-red-600' : 'font-semibold text-green-600'}>
+						{capitalGap > 0
+							? `You are short by $${capitalGap.toLocaleString()}`
+							: `You have $${Math.abs(capitalGap).toLocaleString()} surplus`}
+					</p>
+				</div>
+
+				<button
+					onclick={() => exportToText()}
+					class="mt-4 cursor-pointer rounded-md bg-blue-500 px-4 py-2 text-white"
 				>
-				<input
-					type="number"
-					bind:value={remainingDeposits}
-					placeholder="e.g. 100000"
-					class="w-full rounded-md border border-gray-300 p-3 focus:ring-2 focus:ring-blue-500"
-				/>
+					Download Text File
+				</button>
 			</div>
-
-			<div>
-				<label class="mb-2 block text-sm font-medium" for="solicitors-fees">Solicitors fees:</label>
-				<input
-					type="number"
-					bind:value={solicitorsFees}
-					placeholder="e.g. 100000"
-					class="w-full rounded-md border border-gray-300 p-3 focus:ring-2 focus:ring-blue-500"
-				/>
-			</div>
-
-			<div>
-				<label class="mb-2 block text-sm font-medium" for="stamp-duty"
-					>Stamp duty & other transfer costs:</label
-				>
-				<input
-					type="number"
-					bind:value={stampDuty}
-					placeholder="e.g. 100000"
-					class="w-full rounded-md border border-gray-300 p-3 focus:ring-2 focus:ring-blue-500"
-				/>
-			</div>
-
-			<div>
-				<label class="mb-2 block text-sm font-medium" for="construction-costs"
-					>Construction costs (if not applicable leave at 0):</label
-				>
-				<input
-					type="number"
-					bind:value={constructionCosts}
-					placeholder="e.g. 100000"
-					class="w-full rounded-md border border-gray-300 p-3 focus:ring-2 focus:ring-blue-500"
-				/>
-			</div>
-
-			<div>
-				<label class="mb-2 block text-sm font-medium" for="contingency-capital"
-					>Contingency capital:</label
-				>
-				<input
-					type="number"
-					bind:value={contingencyCapital}
-					placeholder="e.g. 100000"
-					class="w-full rounded-md border border-gray-300 p-3 focus:ring-2 focus:ring-blue-500"
-				/>
-			</div>
-
-			<div>
-				<label class="mb-2 block text-sm font-medium" for="settlement-date"
-					>Next projected property settlement date:</label
-				>
-				<input
-					type="date"
-					bind:value={settlementDate}
-					class="w-full rounded-md border border-gray-300 p-3 focus:ring-2 focus:ring-blue-500"
-				/>
-			</div>
-
-			<div class="space-y-2 rounded-md bg-gray-100 p-4">
-				<p class="font-medium">
-					Amount required for your current strategy: ${totalCapitalRequired}
-				</p>
-				<p>
-					{#if monthsBetween <= 12 && monthsBetween >= 0}
-						Capital required in 12 months: ${capitalRequired12Months.toLocaleString()}
-					{/if}
-				</p>
-				<p>
-					{#if monthsBetween >= 12}
-						Capital required in 24 Months or Moree: ${capitalRequired24Months.toLocaleString()}
-					{/if}
-				</p>
-				<p class={capitalGap > 0 ? 'font-semibold text-red-600' : 'font-semibold text-green-600'}>
-					{capitalGap > 0
-						? `You are short by $${capitalGap.toLocaleString()}`
-						: `You have $${Math.abs(capitalGap).toLocaleString()} surplus`}
-				</p>
-			</div>
-
-			<button
-				onclick={() => exportToText()}
-				class="mt-4 cursor-pointer rounded-md bg-blue-500 px-4 py-2 text-white"
-			>
-				Download Text File
-			</button>
 		</div>
 	</div>
 </main>
